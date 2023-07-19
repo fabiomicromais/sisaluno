54148347e44f45585099b06c9719175946877d17
Fabio Lima <44278903+fabiomicromais@users.noreply.github.com>
2023-07-19 08:15:12 -0300

    Exemplos do crud alunos

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Sisaluno</title>
+</head>
+<body>
+
+   <div class="topo">
+         <p>  sistema acadêmico de alunos estudiosos</p>
+   </div>
+    
+   <div class="content"> 
+       <ul>
+           <li> <a href="./aluno/cadaluno.php">cadastrar</a> </li>  
+           <li> <a href="./aluno/listaalunos.php">listar</a> </li>
+          </ul>
+   </div>
+  
+    
+   <div class="footer">
+      <p> rodape so sistema</p>
+   </div>
+
+</body>
+</html>
